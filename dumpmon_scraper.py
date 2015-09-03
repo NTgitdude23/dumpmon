bff4b4844991147cf24769214e64306d92816285
byt3bl33d3r <byt3bl33d3r@gmail.com>
2015-09-03 10:56:20 +0200

    init

---
+++ dumpmon_scraper.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+from StringIO import StringIO
+from lxml import html
+from time import sleep
+from datetime import datetime
+from dump_stats import Stats
+import os
+import re
+import sys
+import filecmp
+import datetime
+import logging
+import requests
+
+Stats = Stats()
+
+requests_log = logging.getLogger("requests")  #Disables "Starting new HTTP Connection (1)" log message
+requests_log.setLevel(logging.WARNING)
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filename='dumpmon_scraper.log', level=logging.INFO)
+
+logging.info('Starting...')
+
+prev_links = []
+
+while True:
+    
+    try:
+
+        r = requests.get('https://www.twitter.com/dumpmon')
+        tree = html.fromstring(r.text)
+        links = tree.xpath("//a[@class='twitter-timeline-link']//@title")
+        for link in links:
+                if link not in prev_links:
+                    if 'dumpmon.com' in link:
+                        id = link.split('=')[1]
+                    else:
+                        id = None
+
+                    if (id != None):
+                        dump = requests.get(link).text
+                        if re.search('paste has been removed', dump, re.IGNORECASE):
+                            logging.info('Dump id {} has been removed! skipping...'.format(id))
+                            continue
+ 
+                        dump_buffer = StringIO(dump.encode('utf-8', 'ignore'))
+                        dump_name = './dumps/' + id
+
+                        if id in os.listdir('./dumps'):
+                            logging.info('Dump id {} is present in directory'.format(id))
+                            with open('./dumps/' + id, 'r') as file:
+                                d_buffer = file.read(500) 
+                                if d_buffer.encode('utf-8', 'ignore') == dump_buffer.read(500):
+                                    logging.info('Contents of dump id {} appear to be the same! skipping...'.format(id))
+                                    continue
+                                else:   
+                                    logging.info('Contents of dump id {} differ! dumping...'.format(id))
+                                    dump_name = './dumps/{}_{}'.format(id, datetime.strftime('%Y-%m-%d_%H:%M:%S:%s'))
+  
+                        Stats.text = dump.encode('utf-8', 'ignore')
+                        dump_type = Stats.match()
+
+                        log_output = 'New dump detected: %s' % link
+
+                        if dump_type == 'db_dump':
+                            log_output += ' Emails: %s Hashes: %s Keywords: %s' % (Stats.num_emails, Stats.num_hashes, Stats.db_keywords)
+                            if Stats.num_emails > 0 and Stats.num_hashes > 0:
+                                log_output += ' E/H: %s' % str(round(Stats.num_emails / float(Stats.num_hashes), 2))
+
+                        elif dump_type == 'cisco':
+                            log_output += ' Possible Cisco configuration file'
+                        elif dump_type == 'google_api':
+                            log_output += ' Possible Google API key(s)'
+                        elif dump_type == 'honeypot':
+                            log_output += ' Possible Dionaea Honeypot Log'
+                        elif dump_type == 'pgp_private':
+                            log_output += ' Possible PGP private key'
+                        elif dump_type == 'ssh_private':
+                            log_output += ' Possible SSH private key'
+                        elif dump_type == None:
+                            continue
+
+                        with open(dump_name, 'w') as file:
+                            file.write(dump.encode('utf-8', 'ignore'))
+                            file.close()
+
+                        logging.info(log_output)
+
+    except Exception, e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        logging.warning((str(e), exc_type, fname, exc_tb.tb_lineno))
+
+    prev_links = links
+    sleep(10)
--- tools/autopep8-project.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-find . -name "*.py" -or -name "settings.py-example" | xargs autopep8 -i
-
-
